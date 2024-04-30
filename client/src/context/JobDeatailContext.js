d88acf89ed22ccfbc2c44c2eef20618baa75a563
Muneeb Ur Rehman <muneebsurrehman@gmail.com>
2024-05-01 01:36:43 +0500

    replacing url on the search request

--- client/src/context/JobDeatailContext.js
+++ client/src/context/JobDeatailContext.js
@@ -1,42 +1,67 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState } from "react";
 
 const context = createContext();
 
 export const JobDetailState = ({ children }) => {
-    const [what, setwhat] = useState('');
-    const [where, setwhere] = useState('');
-    async function fetchDatafromAPI() {
-      let url = `https://jsearch.p.rapidapi.com/search?query=${what} in ${where}&page=2&num_pages=20`;
+  const [what, setwhat] = useState("");
+  const [where, setwhere] = useState("");
+  const [page, setPage] = useState(1);
+
+  async function fetchDatafromAPI() {
     
-        try {
-    
-          let response = await fetch(url, {
-            method: "GET",
-            headers: {
-              'X-RapidAPI-Key': 'ebcffba3b9msh775b8483c055eabp133e14jsn0200b41cea3c',
-              'X-RapidAPI-Host': 'jsearch.p.rapidapi.com',
-            }
-          });
-          let data = await response.json();
-    
-          setAllJobsData(data.data);
-          console.log(data);
-        
-        } catch (error) {
-          console.log(error);
-        }
-      }
 
-   
-    const [jobDetails , setJobDetails] = useState({
-    });
-    const [allJobsData, setAllJobsData]= useState([]);
-   
-    return (
-        <context.Provider value={{ setwhat, setwhere,what, where,fetchDatafromAPI,jobDetails, setJobDetails, allJobsData,setAllJobsData }}> {children} </context.Provider>
-    )
+    let url = `https://jsearch.p.rapidapi.com/search?query=${what} in ${where}&page=${page}&num_pages=20`;
 
-}
+    try {
+      let response = await fetch(url, {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key":
+            "58ce39cc1amsha4354a3355d61d5p15a347jsn0f6c11dcc7da",
+          "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+        },
+      });
+      const queryParameters = new URLSearchParams(window.location.search);
+          queryParameters.set("what", what);
+          queryParameters.set("where", where);
+          queryParameters.set("page", page);
+          window.history.replaceState(
+            {},
+            "",
+            `${window.location.pathname}?${queryParameters}`
+      );
+      
+      let data = await response.json();
 
+      setAllJobsData(data.data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-export const useJobDetailContext = () => useContext(context); 
+  const [jobDetails, setJobDetails] = useState({});
+
+  const [allJobsData, setAllJobsData] = useState([]);
+
+  return (
+    <context.Provider
+      value={{
+        setwhat,
+        setwhere,
+        what,
+        where,
+        fetchDatafromAPI,
+        jobDetails,
+        setJobDetails,
+        allJobsData,
+        setAllJobsData,
+      }}
+    >
+      {" "}
+      {children}{" "}
+    </context.Provider>
+  );
+};
+
+export const useJobDetailContext = () => useContext(context);
