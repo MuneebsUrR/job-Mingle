c7ce8ba3382d48c6430807c00b83cfa1575a1f69
Muneeb Ur Rehman <muneebsurrehman@gmail.com>
2024-05-01 01:49:10 +0500

    settting the starting path

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Navbar from './components/Navbar';
 import Register from './components/Register';
 import { Route, Routes } from 'react-router-dom';
 import { JobDetailState } from './context/JobDeatailContext';
-import TrendingJobs from './components/TrendingJobs';
+
 function App() {
   return (
     <>
@@ -16,9 +16,9 @@ function App() {
       <Routes>
         <Route exact path='/register' element={<Register />} />
         <Route exact path='/login' element={<Login />} />
-        <Route exact path='/' element={<Home />} />
+        <Route path='/' element={<Redirect to="/home" />} />
       </Routes>
-        {/* <TrendingJobs/> */}
+        
       </JobDetailState>
     </>
   );
